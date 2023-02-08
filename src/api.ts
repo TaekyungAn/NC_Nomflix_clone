da2cd2990ee30fee6e6d038ae32627fcd6762fc8
atk0426@naver.com <taekyung@DESKTOP-HRK1UQR.localdomain>
2023-02-08 15:06:13 +0900

    9.6 Home Screen part Two

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,24 @@
 const API_KEY = "f5618a2de5acaca0d5b54b520c5fa640";
 const BASE_PATH = "https://api.themoviedb.org/3";
-const IMG_PATH = "https://image.tmdb.org/t/p";
+
+interface IMovie {
+  id: number;
+  backdrop_path: string;
+  poster_path: string;
+  title: string;
+  overview: string;
+}
+
+export interface IGetMoviesResult {
+  dates: {
+    maximum: string;
+    minimum: string;
+  };
+  page: number;
+  results: IMovie[];
+  total_pages: number;
+  total_result: number;
+}
 
 export function getMovies() {
   return fetch(`${BASE_PATH}/movie/now_playing?api_key=${API_KEY}`).then(
